6add9493adfebc1271eed7c9ce05b4c3210917b0
kaalam <kaalam@dybot.com>
2018-04-23 22:21:31 +0200

    automated commit, Mon Apr 23 22:21:31 CEST 2018

--- develop_jazz02/search/all_4.js
+++ develop_jazz02/search/all_4.js
@@ -6,5 +6,6 @@ var searchData=
   ['decrease_5flevel',['decrease_level',['../classjazz__containers_1_1AATBlockQueue.html#a3c31288554cf5216b39e5f59683fcf43',1,'jazz_containers::AATBlockQueue']]],
   ['deletesource',['deleteSource',['../structparsedURLhea.html#ad5eaf425feaba1400af763c751f36f27',1,'parsedURLhea']]],
   ['destroy_5fkeeprs',['destroy_keeprs',['../classjazz__containers_1_1JazzBlockKeepr.html#ab34ede7a4ae92dd4fffafded935ce099',1,'jazz_containers::JazzBlockKeepr']]],
-  ['dim',['dim',['../unionjazz__datablocks_1_1JazzTensorDim.html#a7bc4dd53c5f5d5a003d584fa191249a6',1,'jazz_datablocks::JazzTensorDim']]]
+  ['dim',['dim',['../unionjazz__datablocks_1_1JazzTensorDim.html#a7bc4dd53c5f5d5a003d584fa191249a6',1,'jazz_datablocks::JazzTensorDim']]],
+  ['discrete_5frecency',['discrete_recency',['../classjazz__containers_1_1AATBlockQueue.html#afecc18ef12f0c627c2fcaaf4a13967e7',1,'jazz_containers::AATBlockQueue']]]
 ];
--- develop_jazz02/search/variables_4.js
+++ develop_jazz02/search/variables_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['data',['data',['../structblock__C__BOOL.html#a3ea96e1728bb066515122ec26b1e5799',1,'block_C_BOOL::data()'],['../structblock__C__OFFS__CHARS.html#ae4553168c43c04489013e495124effdd',1,'block_C_OFFS_CHARS::data()'],['../structblock__C__INTEGER.html#a847debd9ef5d3936a8e329d9904ab8fc',1,'block_C_INTEGER::data()'],['../structblock__C__REAL.html#a8fe1b3ba724d146666f447ac53890b20',1,'block_C_REAL::data()'],['../structblock__C__RAW.html#aadf94e061bb365c401292087cf06e9ee',1,'block_C_RAW::data()']]],
   ['deletesource',['deleteSource',['../structparsedURLhea.html#ad5eaf425feaba1400af763c751f36f27',1,'parsedURLhea']]],
-  ['dim',['dim',['../unionjazz__datablocks_1_1JazzTensorDim.html#a7bc4dd53c5f5d5a003d584fa191249a6',1,'jazz_datablocks::JazzTensorDim']]]
+  ['dim',['dim',['../unionjazz__datablocks_1_1JazzTensorDim.html#a7bc4dd53c5f5d5a003d584fa191249a6',1,'jazz_datablocks::JazzTensorDim']]],
+  ['discrete_5frecency',['discrete_recency',['../classjazz__containers_1_1AATBlockQueue.html#afecc18ef12f0c627c2fcaaf4a13967e7',1,'jazz_containers::AATBlockQueue']]]
 ];
