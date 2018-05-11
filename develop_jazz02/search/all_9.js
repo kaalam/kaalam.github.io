f3fe3ccafe65b3b8388f2dc373b0be1be1f00026
kaalam <kaalam@dybot.com>
2018-05-11 23:04:28 +0200

    automated commit, Fri May 11 23:04:28 CEST 2018

--- develop_jazz02/search/all_9.js
+++ develop_jazz02/search/all_9.js
@@ -18,6 +18,7 @@ var searchData=
   ['intsxp',['INTSXP',['../jazz01__commons_8h.html#a8c05b2e1095fb38bdf2fa97eabe773d6',1,'jazz01_commons.h']]],
   ['ipr',['ipr',['../structIPrange.html#aaa44a478fdff7c3ec991c3fb0b260b7e',1,'IPrange']]],
   ['iprange',['IPrange',['../structIPrange.html',1,'']]],
+  ['is_5fin_5ftree',['is_in_tree',['../classjazz__containers_1_1AATBlockQueue.html#a103208ee455b10d6a386a8470577ea53',1,'jazz_containers::AATBlockQueue']]],
   ['is_5flast_5fkey',['is_last_key',['../classjzzBLOCKS.html#a96957672c632368038a0b090c7bb4d8a',1,'jzzBLOCKS']]],
   ['isbig',['isBig',['../structstring__buffer.html#aab3f1d1cdc67371408163623f48e0638',1,'string_buffer']]],
   ['isinstrumental',['isInstrumental',['../structparsedURLhea.html#aaf6b748e2095c7b8fd0d554dbee10404',1,'parsedURLhea']]],
--- develop_jazz02/search/functions_11.js
+++ develop_jazz02/search/functions_11.js
@@ -3,6 +3,7 @@ var searchData=
   ['r_5fisna',['R_IsNA',['../jazz01__commons_8h.html#a44f6e16a8c26cb2bbe62804e55d4f1e2',1,'jazz01_commons.h']]],
   ['r_5fvalueofna',['R_ValueOfNA',['../jazz__commons_8cpp.html#adba7de256829917ca0ed5b22a402550b',1,'R_ValueOfNA():&#160;jazz_commons.cpp'],['../namespacejazz__datablocks.html#aba3faeb0c34be4885b660bd3debe14a1',1,'jazz_datablocks::R_ValueOfNA()']]],
   ['realloc_5fc_5foffs_5fchars',['realloc_C_OFFS_CHARS',['../classjzzBLOCKS.html#ac53acaefd5beecc4e32fabe47ae48981',1,'jzzBLOCKS']]],
+  ['rebalance',['rebalance',['../classjazz__containers_1_1AATBlockQueue.html#a745b4e957db2c726472cac312beeed19',1,'jazz_containers::AATBlockQueue']]],
   ['recursive_5fdestroy_5fkeeprs',['recursive_destroy_keeprs',['../classjazz__containers_1_1AATBlockQueue.html#a23c3155ece3d0db74eeb7ba578007354',1,'jazz_containers::AATBlockQueue']]],
   ['register_5fservice',['register_service',['../classjazzServices.html#a5ba027c4e805ba3eab3ec7e533d6310c',1,'jazzServices::register_service()'],['../jazz01__main_8cpp.html#a53becd3885d25236cb4bee4e6a672ada',1,'register_service():&#160;jazz01_main.cpp']]],
   ['reinterpret_5fcast_5fblock',['reinterpret_cast_block',['../classjzzBLOCKS.html#a99a2f45f0fcfacfb3a4923b4add5a8d1',1,'jzzBLOCKS']]],
@@ -16,6 +17,7 @@ var searchData=
   ['remote_5fnew_5fsource',['remote_new_source',['../classjazzCommons.html#a6ff37da61f537be33eb5ec023d166160',1,'jazzCommons']]],
   ['remote_5ftesting_5fpoint',['remote_testing_point',['../classjazzCommons.html#a30cb41a5b56b0ea52bb8181f51ad72af',1,'jazzCommons']]],
   ['remove',['remove',['../classjazz__containers_1_1AATBlockQueue.html#ae230fb5367938b71926a0b47ecf95d4e',1,'jazz_containers::AATBlockQueue']]],
+  ['remove_5fgo_5fdeep',['remove_go_deep',['../classjazz__containers_1_1AATBlockQueue.html#a2c0bb01263fbcc3bfab9b07a08165130',1,'jazz_containers::AATBlockQueue']]],
   ['remove_5fjazz_5fblock',['remove_jazz_block',['../classjazz__persistence_1_1JazzPersistence.html#a7a156999d0fec9209af4d05a173750e9',1,'jazz_persistence::JazzPersistence']]],
   ['return_5ferror_5fmessage',['return_error_message',['../classjzzAPI.html#a4d4f8b6d1807c79c8206b4e22760eb73',1,'jzzAPI']]]
 ];
