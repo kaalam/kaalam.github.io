c79086d660602a41b28b92dee4d75d2d6383c030
kaalam <kaalam@dybot.com>
2018-05-15 23:41:04 +0200

    automated commit, Tue May 15 23:41:04 CEST 2018

--- develop_jazz02/search/defines_5.js
+++ develop_jazz02/search/defines_5.js
@@ -5,8 +5,9 @@ var searchData=
   ['included_5fjazz_5felements_5fclasses',['INCLUDED_JAZZ_ELEMENTS_CLASSES',['../jazz__classes_8h.html#a99c80fdd3f939aef3823bdf82d7964dc',1,'jazz_classes.h']]],
   ['included_5fjazz_5felements_5fcontainers',['INCLUDED_JAZZ_ELEMENTS_CONTAINERS',['../jazz__containers_8h.html#a5071814446b8b7f4b5cf61f04032efd6',1,'jazz_containers.h']]],
   ['included_5fjazz_5felements_5fdatablocks',['INCLUDED_JAZZ_ELEMENTS_DATABLOCKS',['../jazz__datablocks_8h.html#a8bc55e493927b18d575a8d28f1335e91',1,'jazz_datablocks.h']]],
+  ['included_5fjazz_5felements_5fhttpclient',['INCLUDED_JAZZ_ELEMENTS_HTTPCLIENT',['../jazz__httpclient_8h.html#a7fc3cbcef866fa496523ef55ba0e5c8c',1,'jazz_httpclient.h']]],
   ['included_5fjazz_5felements_5fpersistence',['INCLUDED_JAZZ_ELEMENTS_PERSISTENCE',['../jazz__persistence_8h.html#a5e0eaf5d0a6d5f645acf6a49029b3f08',1,'jazz_persistence.h']]],
-  ['included_5fjazz_5felements_5fprimitives',['INCLUDED_JAZZ_ELEMENTS_PRIMITIVES',['../jazz__primitives_8h.html#aa1c73fdb8853140ed0d35bc640bc85cc',1,'jazz_primitives.h']]],
+  ['included_5fjazz_5felements_5fstdcore',['INCLUDED_JAZZ_ELEMENTS_STDCORE',['../jazz__stdcore_8h.html#a4a34ac949d6e3491b79aba93847da875',1,'jazz_stdcore.h']]],
   ['included_5fjazz_5felements_5futils',['INCLUDED_JAZZ_ELEMENTS_UTILS',['../jazz__utils_8h.html#a3855f9a3712280c1ba7f928b3371c3b1',1,'jazz_utils.h']]],
   ['included_5fjazz_5fmain_5frestapi',['INCLUDED_JAZZ_MAIN_RESTAPI',['../jazz__restapi_8h.html#af8322f5e4f43cc832aa40ab7a7a84a4e',1,'jazz_restapi.h']]],
   ['intsxp',['INTSXP',['../jazz01__commons_8h.html#a8c05b2e1095fb38bdf2fa97eabe773d6',1,'jazz01_commons.h']]]
--- develop_jazz02/search/namespaces_0.js
+++ develop_jazz02/search/namespaces_0.js
@@ -3,8 +3,9 @@ var searchData=
   ['jazz_5fclasses',['jazz_classes',['../namespacejazz__classes.html',1,'']]],
   ['jazz_5fcontainers',['jazz_containers',['../namespacejazz__containers.html',1,'']]],
   ['jazz_5fdatablocks',['jazz_datablocks',['../namespacejazz__datablocks.html',1,'']]],
+  ['jazz_5fhttpclient',['jazz_httpclient',['../namespacejazz__httpclient.html',1,'']]],
   ['jazz_5fpersistence',['jazz_persistence',['../namespacejazz__persistence.html',1,'']]],
-  ['jazz_5fprimitives',['jazz_primitives',['../namespacejazz__primitives.html',1,'']]],
   ['jazz_5frestapi',['jazz_restapi',['../namespacejazz__restapi.html',1,'']]],
+  ['jazz_5fstdcore',['jazz_stdcore',['../namespacejazz__stdcore.html',1,'']]],
   ['jazz_5futils',['jazz_utils',['../namespacejazz__utils.html',1,'']]]
 ];
