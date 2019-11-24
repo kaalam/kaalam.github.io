5f5c513f785d798ed7ee46a8402d744590da674b
kaalam <kaalam@kaalam.ai>
2019-11-24 22:21:42 +0100

    automated commit, Sun 24 Nov 2019 10:21:42 PM CET

--- develop_jazz02/search/all_0.js
+++ develop_jazz02/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['_5f_5fattribute_5f_5f',['__attribute__',['../namespacejazz__stdcore.html#a12335a1a23f3b6dcc7a82698babd2862',1,'jazz_stdcore::__attribute__()'],['../namespacejazz__stdcore.html#a002fcbf79172e40ff4a241ffec734033',1,'jazz_stdcore::__attribute__((packed))']]],
-  ['_5fkeepr_5flock_5f',['_keepr_lock_',['../classjazz__containers_1_1JazzBlockKeepr.html#a350d704aa3d0a7a9d9a6f57d946cb67a',1,'jazz_containers::JazzBlockKeepr']]]
+  ['main_2edox',['main.dox',['../main_8dox.html',1,'']]]
 ];
--- develop_jazz02/search/searchdata.js
+++ develop_jazz02/search/searchdata.js
@@ -1,36 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijklmnoprstuvw~",
-  1: "abdfjprs",
-  2: "j",
-  3: "jm",
-  4: "_abcdefghijlmnoprstv~",
-  5: "_abcdefhjklmnoprstuw",
-  6: "ajpt",
-  7: "bcijlmnrst"
+  0: "m",
+  1: "m"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "defines"
+  1: "files"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Data Structures",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Macros"
+  1: "Files"
 };
 
