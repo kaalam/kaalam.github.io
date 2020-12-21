964fbdbb13d9f2b432328301f4fdba2700ecbbf6
kaalam <kaalam@kaalam.ai>
2020-12-21 23:53:52 +0100

    automated commit, Mon 21 Dec 2020 11:53:52 PM CET

--- develop_jazz02/search/functions_13.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_7ehttpserver_335',['~HttpServer',['../classjazz__main_1_1HttpServer.html#a3c75fd69bf2ef99c17c32e67ba84c98a',1,'jazz_main::HttpServer']]],
-  ['_7elogger_336',['~Logger',['../classjazz__elements_1_1Logger.html#a710c1cfbf72eb6a38faad7198bdd0543',1,'jazz_elements::Logger']]]
-];
