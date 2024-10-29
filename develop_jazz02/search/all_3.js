dd807971fb14bc5daf9fcee505df6f094ee808d6
kaalam <kaalam@kaalam.ai>
2024-10-29 21:51:58 +0100

    automated commit, Tue Oct 29 09:51:58 PM CET 2024

--- develop_jazz02/search/all_3.js
+++ develop_jazz02/search/all_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['base_0',['base',['../structjazz__bebop_1_1ApiQueryState.html#abdef11b0e73af02eea30344bad7204f6',1,'jazz_bebop::ApiQueryState::base()'],['../structjazz__elements_1_1Locator.html#a2bf2ec77ab816dafa947c20cf1854010',1,'jazz_elements::Locator::base()']]],
   ['base_5fnames_1',['base_names',['../classjazz__elements_1_1Channels.html#a4f18c8ef503d1991650c10bcb5fc76f5',1,'jazz_elements::Channels::base_names()'],['../classjazz__elements_1_1Container.html#a94a9f090fc004a0eddfce10a8cd150de',1,'jazz_elements::Container::base_names()'],['../classjazz__elements_1_1Persisted.html#a0d12b480ec90d0548b9a069bd88997e9',1,'jazz_elements::Persisted::base_names()'],['../classjazz__elements_1_1Volatile.html#ae1b83228aebd8e7571356b3af8895c2d',1,'jazz_elements::Volatile::base_names()']]],
-  ['base_5fserver_2',['base_server',['../namespacejazz__main.html#a08bd832d5fcd6d625150c84cffcde8f7',1,'jazz_main']]],
+  ['base_5fserver_2',['base_server',['../namespacejazz__bebop.html#ae37ea8bb03c7ccd0065ea81a3c5434bc',1,'jazz_bebop']]],
   ['baseapi_3',['BaseAPI',['../classjazz__bebop_1_1BaseAPI.html#a482b43be524ec391d28623cebfd3fa8f',1,'jazz_bebop::BaseAPI::BaseAPI()'],['../classjazz__bebop_1_1BaseAPI.html',1,'jazz_bebop::BaseAPI']]],
   ['basenames_4',['BaseNames',['../namespacejazz__elements.html#aadf1e411ca544a525c767f3e30010202',1,'jazz_elements']]],
   ['big_5fbang_5',['big_bang',['../classjazz__elements_1_1Logger.html#a0eaf7611e788857fdcac0d679bdafb04',1,'jazz_elements::Logger']]],
