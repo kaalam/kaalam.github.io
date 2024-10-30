e56ccb4761818ab04c4313db1d4af44724d297bf
kaalam <kaalam@kaalam.ai>
2024-10-30 22:02:11 +0100

    automated commit, Wed Oct 30 10:02:11 PM CET 2024

--- develop_jazz02/search/variables_2.js
+++ develop_jazz02/search/variables_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['base_0',['base',['../structjazz__bebop_1_1ApiQueryState.html#abdef11b0e73af02eea30344bad7204f6',1,'jazz_bebop::ApiQueryState::base()'],['../structjazz__elements_1_1Locator.html#a2bf2ec77ab816dafa947c20cf1854010',1,'jazz_elements::Locator::base()']]],
-  ['base_5fserver_1',['base_server',['../namespacejazz__bebop.html#ae37ea8bb03c7ccd0065ea81a3c5434bc',1,'jazz_bebop']]],
+  ['base_5fserver_1',['base_server',['../classjazz__bebop_1_1BaseAPI.html#a60ffc06721ef1168a3e76939fc2ca1e3',1,'jazz_bebop::BaseAPI']]],
   ['big_5fbang_2',['big_bang',['../classjazz__elements_1_1Logger.html#a0eaf7611e788857fdcac0d679bdafb04',1,'jazz_elements::Logger']]],
   ['buffer_3',['buffer',['../structjazz__elements_1_1StringBuffer.html#aad4fda4c16c6eddb4ff3b2c56079bca1',1,'jazz_elements::StringBuffer']]],
   ['buffer_5fsize_4',['buffer_size',['../structjazz__elements_1_1StringBuffer.html#a8bf233a47a69a90211852992d604f25b',1,'jazz_elements::StringBuffer']]]
