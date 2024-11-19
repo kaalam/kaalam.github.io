41c2ff0aec2bb790cc7e296ac6ff4100f694786f
kaalam <kaalam@kaalam.ai>
2024-11-19 20:27:06 +0100

    automated commit, Tue Nov 19 08:27:06 PM CET 2024

--- develop_jazz02/search/classes_2.js
+++ develop_jazz02/search/classes_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['channels_0',['Channels',['../classjazz__elements_1_1Channels.html',1,'jazz_elements']]],
-  ['concept_1',['Concept',['../classjazz__models_1_1Concept.html',1,'jazz_models']]],
-  ['configfile_2',['ConfigFile',['../classjazz__elements_1_1ConfigFile.html',1,'jazz_elements']]],
-  ['container_3',['Container',['../classjazz__elements_1_1Container.html',1,'jazz_elements']]],
-  ['core_4',['Core',['../classjazz__bebop_1_1Core.html',1,'jazz_bebop']]]
+  ['colselection_1',['ColSelection',['../classjazz__bebop_1_1ColSelection.html',1,'jazz_bebop']]],
+  ['concept_2',['Concept',['../classjazz__models_1_1Concept.html',1,'jazz_models']]],
+  ['configfile_3',['ConfigFile',['../classjazz__elements_1_1ConfigFile.html',1,'jazz_elements']]],
+  ['container_4',['Container',['../classjazz__elements_1_1Container.html',1,'jazz_elements']]],
+  ['core_5',['Core',['../classjazz__bebop_1_1Core.html',1,'jazz_bebop']]]
 ];
--- develop_jazz02/search/functions_13.js
+++ develop_jazz02/search/functions_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_7elogger_0',['~Logger',['../classjazz__elements_1_1Logger.html#a710c1cfbf72eb6a38faad7198bdd0543',1,'jazz_elements::Logger']]]
+  ['where_0',['where',['../classjazz__bebop_1_1Space.html#a4546064eeaa63a9a2efee8790cfe16f8',1,'jazz_bebop::Space']]]
 ];
