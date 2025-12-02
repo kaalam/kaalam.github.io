4d2058b2179583aec28e9824a3edf862b16fa994
sbasaldua <santiago.basaldua@bbvadata.com>
2025-12-02 21:44:40 +0100

    automated commit, Tue Dec  2 09:44:40 PM CET 2025

--- develop_jazz02/search/classes_12.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['tensor_0',['Tensor',['../unionjazz__elements_1_1Tensor.html',1,'jazz_elements']]],
-  ['tensordim_1',['TensorDim',['../structjazz__elements_1_1TensorDim.html',1,'jazz_elements']]],
-  ['tensortype_2',['TensorType',['../structjazz__bebop_1_1TensorType.html',1,'jazz_bebop']]],
-  ['transaction_3',['Transaction',['../structjazz__elements_1_1Transaction.html',1,'jazz_elements']]],
-  ['tuple_4',['Tuple',['../classjazz__elements_1_1Tuple.html',1,'jazz_elements']]]
-];
